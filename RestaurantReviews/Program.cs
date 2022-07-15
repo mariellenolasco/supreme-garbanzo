b75519aca21446e763b9488750f5a517533f85e7
Marielle Nolasco <marielletnolasco@gmail.com>
2022-07-15 13:18:41 -0400

    added restaurant class

--- RestaurantReviews/Program.cs
+++ RestaurantReviews/Program.cs
@@ -9,7 +9,8 @@ do
     // present menu to end user
     Console.WriteLine("Welcome to my restaurant reveiws app! The essential app for the amateur foodie");
     Console.WriteLine("[1] See that welcome message again");
-    Console.WriteLine("[2] Exit");
+    Console.WriteLine("[2] Add Restaurant");
+    Console.WriteLine("[x] Exit");
 
     // get user input
     // create a string called userInput to store the value of the user's choice. We read the user's choice from the console 
@@ -26,6 +27,22 @@ do
             Console.WriteLine("Hello World!");
             break;
         case "2":
+            // lhs Restaurant defines the type of the variable 
+            // lhs newRestaurant defines the name of the variable
+            Restaurant newRestaurant = new Restaurant();
+            //rhs new keyword refers to creating a new object and setting aside space for it in memory
+            // rhs Restaurant() refers to the constructor of the Restaurant class
+
+            //set restaurant properties
+            newRestaurant.Name = "Balajadia's Eatery";
+            newRestaurant.City = "Baguio City";
+            newRestaurant.State = "Benguet";
+            newRestaurant.Rating = 5;
+            newRestaurant.Description = "Ilocano cuisine at it's finest. Fresh meat from the slaughterhouse next door";
+            // print my new restaurant
+            Console.WriteLine(newRestaurant);
+            break;
+        case "x":
             //exit out of program
             // set the runAgain value to false to exit out of the loop
             runAgain = false;
---
+++ RestaurantReviews/Restaurant.cs
@@ -0,0 +1,29 @@
+class Restaurant
+{
+    // 3 parts of a class (in general)
+    // Properties
+    // In C# properties are separate from fields
+    // fields hold value
+    // properties provide a way to add logic in getting
+    // or setting the field value
+    // properties are the c# equivalent of a getter and 
+    // setter in java
+    // You don't have to define backing field for properties
+    // if you don't require any special logic to access 
+    // or set their value, you can just declare a property
+    // and the compiler generates a backing field (aka variable
+    // that holds actual data) for you
+    // Methods
+    // Constructors
+
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public int Rating { get; set; }
+    public string Description { get; set; }
+
+    public override string ToString()
+    {
+        return $"Name: {this.Name} \nAddress: {this.City}, {this.State} \nRating: {this.Rating} / 5 \n{this.Description}";
+    }
+}
