8ba1d5f13887648c72e4040e8a4e7547a6699da9
Marielle Nolasco <marielletnolasco@gmail.com>
2022-07-15 12:27:15 -0400

    morning part of the lecture

---
+++ CSharp.md
@@ -0,0 +1,15 @@
+# C#
+
+A object oriented programming language developed by Microsoft. Because it's a Microsoft baby, it's .Net compliant.
+
+## .NET
+
+A development platform. A collection of languages, libraries, frameworks that allow for development of different types of apps, like games, webapps, etc.
+
+## SDK vs Runtime
+
+SDK (software development kit) vs runtime. You use SDK to develop (aka write code) and run it. You use runtime to run apps. SDKs have runtime.
+
+## The process of running code
+
+You have a human readable .cs file this gets compiled by .net compiler platform into Intermediate language (IL). This gets compiled by the CLR specifically the JIT compiler into machine/native code. The machine code gets executed by machine.
---
+++ RestaurantReviews/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// this is used to check whether or not the end user wishes to exit out of the program
+Boolean runAgain = true;
+// Loop through the options at least once to present them to the end user, if the end user does not wish to exit, keep
+// presenting this list of options
+do
+{
+    // present menu to end user
+    Console.WriteLine("Welcome to my restaurant reveiws app! The essential app for the amateur foodie");
+    Console.WriteLine("[1] See that welcome message again");
+    Console.WriteLine("[2] Exit");
+
+    // get user input
+    // create a string called userInput to store the value of the user's choice. We read the user's choice from the console 
+    // using the readline method
+    string userInput = Console.ReadLine();
+
+    // using string interpolation we're able to present what the user chose
+    Console.WriteLine($"You chose {userInput}");
+
+    // using switch code block we define multiple paths of logic depending on the value of our user input
+    switch (userInput)
+    {
+        case "1":
+            Console.WriteLine("Hello World!");
+            break;
+        case "2":
+            //exit out of program
+            // set the runAgain value to false to exit out of the loop
+            runAgain = false;
+            Console.WriteLine("Goodbye Cruel World...");
+            break;
+        default:
+            Console.WriteLine("Sorry your input was invalid, try again");
+            break;
+    }
+} while (runAgain);
+
+
